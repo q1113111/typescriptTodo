a1be9112c4345aeb9c9fb32a22f4f42208a1797a
DESKTOP-QQU53VF\ann <q1113111@gmail.com>
2021-10-27 13:20:16 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
+import path from 'path' // 主要用于alias文件路径别名
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue(),
+    Components({
+      resolvers: [
+        ElementPlusResolver({
+          importStyle: 'sass'
+        })
+      ]
+    })],
+  resolve: {
+    alias: {
+      '@/': `${path.resolve(__dirname, 'src')}/`,
+      '~/': `${path.resolve(__dirname, './')}/`
+    }
+  }
 })
