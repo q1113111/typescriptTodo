1fd40637eb663c8114a809eaef8372e1ee12dd43
DESKTOP-QQU53VF\ann <q1113111@gmail.com>
2021-10-28 13:34:05 +0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: ['./public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}'],
+  darkMode: false, // or 'media' or 'class'
+  mode: 'jit',
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
