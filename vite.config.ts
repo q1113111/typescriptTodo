7a17dbd4e0f3d6dd5e5ab9759d3e476840d43cd5
Lai Sam <q1113111@gmail.com>
2021-10-30 01:28:57 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
 import { defineConfig } from 'vite'
 import path from 'path' // 主要用于alias文件路径别名
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import ViteRestart from 'vite-plugin-restart'
+import { injectHtml } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+    Components(),
+    ViteRestart({
+      restart: [
+        'my.config.ts'
+      ]
+    }),
+    injectHtml({
+      data: {
+        title: 'my-app'
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@/': `${path.resolve(__dirname, 'src')}/`,
